4ae9d41759e5ba705847e235ba2321290b6c5819
marusheep <38782965+marusheep@users.noreply.github.com>
2020-12-15 23:04:43 +0000

    new file

---
+++ revising-the-select-query.sql
@@ -0,0 +1,7 @@
+/* https://www.hackerrank.com/challenges/revising-the-select-query/problem */
+
+SELECT * FROM CITY
+    WHERE POPULATION > 100000 AND COUNTRYCODE = "USA";
+
+/* https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?h_r=next-challenge&h_v=zen */
+
