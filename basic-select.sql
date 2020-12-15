ccb20950a428138a948649ba150285bb00a7350e
marusheep <38782965+marusheep@users.noreply.github.com>
2020-12-15 23:32:17 +0000

    NEW

---
+++ basic-select.sql
@@ -0,0 +1,51 @@
+/* https://www.hackerrank.com/challenges/revising-the-select-query/problem */
+
+SELECT * FROM CITY
+    WHERE POPULATION > 100000 AND COUNTRYCODE = "USA";
+
+/* https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?h_r=next-challenge&h_v=zen */
+
+SELECT NAME FROM CITY
+    WHERE COUNTRYCODE = "USA" AND POPULATION > 120000;
+
+/* https://www.hackerrank.com/challenges/select-all-sql/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen */
+
+SELECT * FROM CITY;
+
+/* https://www.hackerrank.com/challenges/select-by-id/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen */
+
+SELECT * FROM CITY
+    WHERE ID = 1661;
+
+
+/* 
+Japanese Cities' Attributes
+https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+*/
+
+SELECT * FROM CITY
+    WHERE COUNTRYCODE = "JPN";
+
+/*
+Japanese Cities' Names
+https://www.hackerrank.com/challenges/japanese-cities-name/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+*/
+
+SELECT NAME FROM CITY
+    WHERE COUNTRYCODE = "JPN"
+
+/*
+Weather Observation Station 1
+https://www.hackerrank.com/challenges/weather-observation-station-1/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+*/
+
+SELECT CITY,STATE
+    FROM STATION;
+
+/*
+Weather Observation Station 3
+https://www.hackerrank.com/challenges/weather-observation-station-3/problem?h_r=next-challenge&h_v=zen
+*/
+SELECT DISTINCT(CITY) FROM STATION
+    WHERE ID%2 = 0;
+
