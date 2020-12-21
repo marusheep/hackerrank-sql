3f8791bd6235f28dc1489dec8520cf1dae4591a3
marusheep <38782965+marusheep@users.noreply.github.com>
2020-12-21 10:42:47 +0000

    update

--- advanced-join.sql
+++ advanced-join.sql
@@ -7,8 +7,4 @@ Where P2.Salary > P1.Salary
 Order By P2.Salary;
 
 -- Symmetric Pairs (https://www.hackerrank.com/challenges/symmetric-pairs/)
-SELECT f1.X, f1.Y FROM Functions f1 -- f1, 1st pair of x,y
-    INNER JOIN Functions f2 ON f1.X=f2.Y AND f1.Y=f2.X --f2, symmetric pairs X1 = Y2 and X2 = Y1
-    GROUP BY f1.X, f1.Y 
-        HAVING COUNT(f1.X)>1 or f1.X<f1.Y
-    ORDER BY f1.X 
+
