c0a832900c922f9ffc7ee8f12128603114321722
marusheep <marusheep@gmail.com>
2022-02-23 19:50:32 +0000

    NEW

---
+++ basic-join-population-census.sql
@@ -0,0 +1,43 @@
+/*
+Given the "CITY" and "COUNTRY" tables, query the sum of the population of all cities where the 'CONTINENT' is 'Asia'.
+
+Note: CITY.CountryCode and COUNTRY.CountryCode are matching key columns.
+
+Input Format
+
+The CITY and COUNTRY table are described as below:
+
+CITY
+
+| Field       | Type         |
+|-------------|--------------|
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
+
+COUNTRY
+
+| Field          | Type         |
+|----------------|--------------|
+| CODE           | VARCHAR2(3)  |
+| NAME           | VARCHAR2(44) |
+| CONTINENT      | VARCHAR2(13) |
+| REGION         | VARCHAR2(25) |
+| SURFACEAREA    | NUMBER       |
+| INDEPYEAR      | VARCHAR2(5)  |
+| POPULATION     | NUMBER       |
+| LIFEEXPENTANCY | VARCHAR2(4)  |
+| GNP            | NUMBER       |
+| GNPOLD         | VARCHAR2(9)  |
+| LOCALNAME      | VARCHAR2(44) |
+| GOVERNMENTFORM | VARCHAR2(44) |
+| HEADOFSTATE    | VARCHAR2(32) |
+| CAPITAL        | VARCHAR2(4)  |
+| CODE2          | VARCHAR2(2)  |
+
+*/
+
+SELECT sum(CITY.POPULATION) FROM CITY, COUNTRY
+    WHERE CITY.COUNTRYCODE =  COUNTRY.CODE AND COUNTRY.CONTINENT = 'Asia';
